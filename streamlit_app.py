66802cebc57cec3a5f421375281fc17021a56832
somenmantra <96628079+somenmantra@users.noreply.github.com>
2023-01-12 23:26:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
-streamlit.title('I am doing the new changes')
-streamlit.header('Application building')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
